512b9a76b45459e971b8adef3f74a693254dcf99
码上talk|RC <3189482282@qq.com>
2022-07-02 10:08:55 +0800

    1、Fixed: fixed the bug bofore deploy

--- backend/config/index.js
+++ backend/config/index.js
@@ -3,8 +3,8 @@ const config = {
     DATABASE: "demo",
     USERNAME: "root",
     PASSWORD: "123456",
-    PORT: "3306",
-    HOST: "127.0.0.1",
+    PORT: "10020",
+    HOST: "8.129.146.48",
   },
 };
 
--- backend/config/index.prod.js
+++ backend/config/index.prod.js
@@ -1,10 +1,10 @@
 const config = {
   mysql: {
-    DATABASE: "demo",
-    USERNAME: "root",
-    PASSWORD: "123456",
-    PORT: "10020",
-    HOST: "8.129.146.48",
+    DATABASE: "",
+    USERNAME: "",
+    PASSWORD: "",
+    PORT: "",
+    HOST: "",
   }
 }
 
